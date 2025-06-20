70cde5487b47bf62bc5a2c2e47871905d925b852
hackermamo <151397014+hackermamo@users.noreply.github.com>
2025-06-20 21:06:01 +0530

    config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('/etc/secrets/.env')
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 LITERARY_API_KEY = os.getenv("LITERARY_API_KEY")
